6ce79133e9d66bb1255aa271de7e1a1e0a2535e5
Joey <joey@givey.co.uk>
2014-03-06 08:32:42 +0000

    Pushing any changes

--- assets/js/router.js
+++ assets/js/router.js
@@ -31,31 +31,6 @@ define([
 			var menuView = new MenuView;
 		});
 
-		// Default/Home route.
-		app_router.on('route:home', function(action) {
-			if(!Session.is_authenticated()) {
-				// Create a new instance of the home view.
-				var homeView = new LoginView;
-			} else {
-				app_router.navigate('dashboard', {trigger: true});
-			}
-		});
-
-		// We're also going to listen to events
-		// fired on the Session model for changes
-		// in user authentication.
-		Session.on('change:api_key', function(Session) {
-			// A user has just logged in.
-			if(Session.get('api_key')) {
-				app_router.navigate('dashboard', {trigger: true});
-			}
-
-			// A user has just logged out.
-			else {
-				app_router.navigate('login', {trigger: true});
-			}
-		});
-
 		// Start tracking history.
 		Backbone.history.start();
 	};
